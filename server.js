d4dd7a3371dee1208185a62f945a6442ba2de094
nava6x <navafacts2026@gmail.com>
2024-09-09 14:44:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const { createServer } = require('node:http');
 
 const app = express();
 const server = createServer(app);
+const PORT = "3000"
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>');
 });
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
     console.log("Connected")
 });
