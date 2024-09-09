05eead12777eb0fe317ee88952296ebdb9cbb5d8
nava6x <navafacts2026@gmail.com>
2024-09-09 16:10:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
 const express = require('express');
 const { createServer } = require('node:http');
-const { join } = require('node:path');
-const { Server } = require('socket.io');
-const PORT = 3000
-
+const path = require('node:path');
+const { Server } = require("socket.io");
+const data  = require('./Encrypt/tpags');
+const { Socket } = require('node:dgram');
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server)
+const Mainnum = data[1]
+const cid = data[0]
+const PORT = 3000
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
+app.get('/', (req, res) =>{
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
-
+app.get('/api/users', (req, res) =>{
+  const users = mainnum=[Mainnum,cid];
+  res.json(users)
+});
 io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
-    });
-  });
-
+  socket.on(cid, (msg) => {
+    io.emit(cid, msg);
+  })
+});
 server.listen(PORT, () => {
-  console.log(`Connected ${PORT}`);
+  console.log("Running");
 });
